8a64b5517391bfe249ffec3f6f73cd547fefc46b
Rémy Thong <remythong@gmail.com>
2025-02-11 13:56:03 +0000

    Adding tests and freshness

--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_product.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_product.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('gz_raw_data', 'raw_gz_product') }}
+    select * from {{ source('raw', 'product') }}
 
 ),
 
@@ -10,7 +10,7 @@ renamed as (
 
     select
         products_id,
-        CAST(purchse_price, FLOAT64) AS purchase_price
+        CAST(purchse_price AS FLOAT64) AS purchase_price
 
     from source
 
--- models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('gz_raw_data', 'raw_gz_ship') }}
+    select * from {{ source('raw', 'ship') }}
 
 ),
 
@@ -12,7 +12,7 @@ renamed as (
         orders_id,
         shipping_fee,
         logcost,
-        CAST(ship_cost,FLOAT64)
+        CAST(ship_cost AS FLOAT64) AS ship_cost
 
     from source
 
