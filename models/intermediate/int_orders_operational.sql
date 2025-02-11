98a5ddfd51fba31f54c4ee1cc6494ac75ecfcb2f
Rémy Thong <remythong@gmail.com>
2025-02-11 16:07:14 +0000

    operational margin

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,8 @@
 SELECT
-    date_date
-    ,orders_id
+    orders_id
+    ,date_date
     ,(margin + shipping_fee) - (logcost + ship_cost) AS operational_margin
+    ,quantity
 FROM {{ ref('int_orders_margin') }}
 JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }}
 USING(orders_id)
