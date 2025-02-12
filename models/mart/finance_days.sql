22a4540c0c9e93bf7982f39f6bc33689f0eda9e8
Rémy Thong <remythong@gmail.com>
2025-02-12 08:11:53 +0000

    Chall 5 correction

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,12 +1,18 @@
-select
-    date_date
-    ,COUNT(DISTINCT(orders_id))  AS nb_transactions
-    ,ROUND(SUM(revenue),1) AS revenue
-    ,ROUND(AVG(revenue),1) AS average_basket
-    ,ROUND(SUM(operational_margin),1) AS operational_margin
-    ,ROUND(SUM(purchase_cost),1) AS purchase_cost
-    ,ROUND(SUM(shipping_fee),1) AS tot_shipping_fees
-    ,ROUND(SUM(logcost),1) AS tot_log_cost
-    ,SUM(quantity) AS tot_products_sold
-FROM {{ ref('int_orders_operational') }}
-GROUP BY date_date
+ -- finance_days.sql
+
+ SELECT
+     date_date
+     ,COUNT(orders_id) AS nb_transactions
+     ,ROUND(SUM(revenue),0) AS revenue
+     ,ROUND(AVG(revenue),1) AS average_basket
+     ,ROUND(SUM(revenue)/COUNT(orders_id),1) AS average_basket_bis
+     ,ROUND(SUM(margin),0) AS margin
+     ,ROUND(SUM(operational_margin),0) AS operational_margin
+     ,ROUND(SUM(purchase_cost),0) AS purchase_cost
+     ,ROUND(SUM(shipping_fee),0) AS shipping_fee
+     ,ROUND(SUM(logcost),0) AS logcost
+     ,ROUND(SUM(ship_cost),0) AS ship_cost
+     ,SUM(quantity) AS quantity
+ FROM {{ ref('int_orders_operational') }}
+ GROUP BY  date_date
+ ORDER BY  date_date DESC
